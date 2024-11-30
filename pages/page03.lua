a11eef9eb2e15165f8fe29e1d58f03207662441e
Rônald Macário <ronaldmacario13@gmail.com>
2024-11-30 12:38:12 -0300

    feat: Add images for cafeteria, gym, and classroom in scene:create

--- pages/page03.lua
+++ pages/page03.lua
@@ -9,4 +9,18 @@ function scene:create(event)
     background.x = display.contentCenterX
     background.y = display.contentCenterY
 
+    -- Áreas da escola
+    local cafeteria = display.newImageRect(sceneGroup, "assets/images/Pag2/Tableware.png", 100, 100)
+    cafeteria.x = display.contentWidth * 0.25
+    cafeteria.y = display.contentHeight * 0.6
+
+    local gym = display.newImageRect(sceneGroup, "assets/images/Pag2/Dumbbell.png", 100
+, 100)
+    gym.x = display.contentWidth * 0.5
+    gym.y = display.contentHeight * 0.6
+
+    local classroom = display.newImageRect(sceneGroup, "assets/images/Pag2/classroom.png", 100
+, 100)
+    classroom.x = display.contentWidth * 0.75
+    classroom.y = display.contentHeight * 0.6
 return scene
