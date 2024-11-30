5654292205d114a5e51d93125d867851798a34c4
Rônald Macário <ronaldmacario13@gmail.com>
2024-11-30 11:02:57 -0300

    feat: Update image dimensions in scene:create for better display and add tap event listener to overlay

--- pages/page02.lua
+++ pages/page02.lua
@@ -51,7 +51,7 @@ function scene:create(event)
     btn1.x = display.contentWidth / 4
     btn1.y = display.contentHeight - 125
 
-    local image1 = display.newImageRect(sceneGroup, "assets/images/Pag1/Frame1.png", 10 , 10)
+    image1 = display.newImageRect(sceneGroup, "assets/images/Pag1/Frame1.png", 300, 300)
     image1.x = display.contentCenterX
     image1.y = display.contentCenterY
     image1.isVisible = false
@@ -65,7 +65,7 @@ function scene:create(event)
     btn2.x = display.contentWidth / 2
     btn2.y = display.contentHeight - 125
 
-    local image2 = display.newImageRect(sceneGroup, "assets/images/Pag1/Frame2.png", 10, 10)
+    image2 = display.newImageRect(sceneGroup, "assets/images/Pag1/Frame2.png", 300, 300)
     image2.x = display.contentCenterX
     image2.y = display.contentCenterY
     image2.isVisible = false
@@ -79,7 +79,7 @@ function scene:create(event)
     btn3.x = 3 * display.contentWidth / 4
     btn3.y = display.contentHeight - 125
 
-    local image3 = display.newImageRect(sceneGroup, "assets/images/Pag1/Frame3.png", 10, 10)
+    image3 = display.newImageRect(sceneGroup, "assets/images/Pag1/Frame3.png", 300, 300)
     image3.x = display.contentCenterX
     image3.y = display.contentCenterY
     image3.isVisible = false
@@ -88,6 +88,9 @@ function scene:create(event)
         toggleImage(image3)
     end)
 
+    -- Adiciona o evento de toque no overlay para fechar a imagem
+    overlay:addEventListener("tap", closeImage)
+
     -- Botão de avançar
     local btnNext = display.newImage(sceneGroup, "assets/images/BtnNext.png")
     btnNext.x = display.contentWidth - 45
@@ -95,60 +98,55 @@ function scene:create(event)
     btnNext:scale(0.8, 0.8)
 
     btnNext:addEventListener("tap", function(event)
-        print("next")
         composer.gotoScene("pages.page03", { effect = "fade" })
     end)
 
     -- Botão de voltar
     local btnPrev = display.newImage(sceneGroup, "assets/images/BtnLeft.png")
-    btnPrev.x = 40 
+    btnPrev.x = 40
     btnPrev.y = display.contentHeight - 40
     btnPrev:scale(0.8, 0.8)
 
     btnPrev:addEventListener("tap", function(event)
-        print("prev")
         composer.gotoScene("pages.Capa")
     end)
 end
 
-function scene:show( event )
-
+function scene:show(event)
     local sceneGroup = self.view
     local phase = event.phase
 
-    if ( phase == "will" ) then
+    if (phase == "will") then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
 
-    elseif ( phase == "did" ) then
+    elseif (phase == "did") then
         -- Code here runs when the scene is entirely on screen
 
     end
 end
 
-function scene:hide( event )
-
+function scene:hide(event)
     local sceneGroup = self.view
     local phase = event.phase
 
-    if ( phase == "will" ) then
+    if (phase == "will") then
         -- Code here runs when the scene is on screen (but is about to go off screen)
 
-    elseif ( phase == "did" ) then
+    elseif (phase == "did") then
         -- Code here runs immediately after the scene goes entirely off screen
 
     end
 end
 
-function scene:destroy( event )
-
+function scene:destroy(event)
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
 
 end
 
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
-scene:addEventListener( "destroy", scene )
+scene:addEventListener("create", scene)
+scene:addEventListener("show", scene)
+scene:addEventListener("hide", scene)
+scene:addEventListener("destroy", scene)
 
 return scene
