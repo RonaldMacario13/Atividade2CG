4e40889be300717ba551cafe2e76454b3f328611
Rônald Macário <ronaldmacario13@gmail.com>
2024-12-01 14:10:12 -0300

    feat: Update page05 to use a background image and remove unused elements

--- pages/page05.lua
+++ pages/page05.lua
@@ -1,50 +1,12 @@
-local composer = require( "composer" )
+local composer = require("composer")
 local scene = composer.newScene()
 
-
-function scene:create( event )
+function scene:create(event)
     local sceneGroup = self.view
 
-    local bg = display.newRect(sceneGroup, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
-    bg:setFillColor(1, 1, 1)
-
-    local pageText = display.newText({
-        parent = sceneGroup,
-        text = "Página 05",
-        x = display.contentWidth - 60,
-        y = 30,
-        font = native.systemFontBold,
-        fontSize = 24,
-    })
-    pageText:setFillColor(0, 0, 0)
-    local btnNext = display.newImage(sceneGroup, "assets/images/BtnNext.png")
-    btnNext.x = display.contentWidth - 45
-    btnNext.y = display.contentHeight - 40 
-    btnNext:scale(0.8, 0.8)
-
-
-    btnNext:addEventListener("tap", function(event)
-        print("next")
-        composer.gotoScene("pages.contraCapa", { effect = "fade" })
-    end)
-
-
-    local btnPrev = display.newImage(sceneGroup, "assets/images/BtnLeft.png")
-    btnPrev.x = 40  
-    btnPrev.y = display.contentHeight - 40 
-
-    btnPrev:scale(0.8, 0.8)
-    btnPrev:addEventListener("tap", function(event)
-        print("prev")
-        composer.gotoScene("pages.page04")
-    end)
-end
-
-
-
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
-scene:addEventListener( "destroy", scene )
+    -- Fundo 
+    local background = display.newImageRect(sceneGroup, "assets/images/Pag4/Pagina5.png", display.contentWidth, display.contentHeight)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
 
 return scene
