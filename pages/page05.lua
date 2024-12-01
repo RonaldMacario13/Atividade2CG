609f7ec5479e8037f6f384672e1e17ca041c3c36
Rônald Macário <ronaldmacario13@gmail.com>
2024-12-01 14:10:41 -0300

    feat: Implement shake effect to energize healthy elements and reduce unhealthy ones on page05

--- pages/page05.lua
+++ pages/page05.lua
@@ -33,4 +33,42 @@ function scene:create(event)
     local hotdog = display.newImageRect(sceneGroup, "assets/images/Pag4/HotDog.png", 70, 70)
     hotdog.x = display.contentWidth * 0.7
     hotdog.y = display.contentHeight * 0.80
+
+    -- Variável de controle para permitir shake
+    local canShake = true
+
+    -- Função de Shake
+    local function onShake(event)
+        if event.isShake and canShake then
+            canShake = false -- Desativa o shake temporariamente
+
+            -- Energiza os elementos saudáveis
+            transition.to(dumbbell, { time = 500, xScale = 1.5, yScale = 1.5, transition = easing.continuousLoop, onComplete = function()
+            transition.to(dumbbell, { time = 500, xScale = 1.0, yScale = 1.0 })
+            end })
+
+            transition.to(watermelon, { time = 500, rotation = 360, transition = easing.outBounce })
+            
+            transition.to(sanitizer, { time = 500, xScale = 1.5, yScale = 1.5, transition = easing.continuousLoop, onComplete = function()
+            transition.to(sanitizer, { time = 500, xScale = 1.0, yScale = 1.0 })
+            end })
+
+
+            -- Reduz os elementos não saudáveis
+            transition.to(pizza, { time = 500, alpha = 0.2 })
+            transition.to(hamburguer, { time = 500, alpha = 0.2 })
+            transition.to(hotdog, { time = 500, alpha = 0.2 })
+
+            -- Reativa o shake e restaura os estados originais após 5 segundos
+            timer.performWithDelay(5000, function()
+                canShake = true
+                -- Restaura elementos saudáveis
+                watermelon.rotation = 0 
+                -- Restaura elementos não saudáveis
+                transition.to(pizza, { time = 500, alpha = 1 })
+                transition.to(hamburguer, { time = 500, alpha = 1 })
+                transition.to(hotdog, { time = 500, alpha = 1 })
+            end)
+        end
+    end
 return scene
